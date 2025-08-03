fb31fee78225b4fc182126612203763881021bd9
boben195 <boben195@ukr.net>
2025-08-03 15:57:43 +0300

    add font chnge global css

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 
 
 export default function Home() {
-  return (<div>
-    <Button>
-      Click Me
-    </Button>
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button>Button</Button>
+      </div>
+      <div>
+        <Input placeholder="Input"></Input>
+      </div>
+      <div>
+        <Progress value={50}></Progress>
+      </div>
+      <div>
+        <Textarea value="Textarea"></Textarea>
+      </div>
   </div>)
 }
