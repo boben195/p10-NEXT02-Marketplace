a1ef955504600ef49e015b516e8aa48d7d9ced6f
boben195 <boben195@ukr.net>
2025-08-04 16:45:44 +0300

    finish with desctop part of navbar

--- my-app/src/app/(home)/navbar.tsx
+++ my-app/src/app/(home)/navbar.tsx
@@ -42,7 +42,6 @@ const navbarItems = [
     { href: "/features", children: "Features" },
     { href: "/pricing", children: "Pricing" },
     { href: "/contact", children: "Contact" },
-
 ]
 
 export const Navbar = () => {
@@ -62,6 +61,14 @@ export const Navbar = () => {
                       {item.children}
                       </NavbarItem>
               ))}
+          </div>
+          <div className="hidden lg:flex">
+              <Button asChild variant="secondary" className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg">
+                 <Link href="/sign-in">Log In</Link>
+              </Button>
+              <Button asChild className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black transition-colors text-lg">
+                 <Link href="/sign-up"> Start Selling</Link>
+              </Button>
           </div>
     </nav>
   )
