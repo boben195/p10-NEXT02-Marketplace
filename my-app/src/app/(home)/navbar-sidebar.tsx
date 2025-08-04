e44ecc527eae7032fab8d954861c4c5f4b7c3971
boben195 <boben195@ukr.net>
2025-08-04 17:38:34 +0300

    finish with sidebar

--- my-app/src/app/(home)/navbar-sidebar.tsx
+++ my-app/src/app/(home)/navbar-sidebar.tsx
@@ -24,21 +24,31 @@ export const NavbarSidebar = ({ items, open, onOpenChange }: Props) => {
         <Sheet open={open} onOpenChange={onOpenChange}>
             <SheetContent side="left" className="p-0 transition-none">
                 <SheetHeader className="p-4 border-b">
-                    <div className="flex items-center">
+                    
                         <SheetTitle>
                             Menu
                         </SheetTitle>
-                    </div>
+                    
                 </SheetHeader>
                 <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
                     {items.map((item) => {
                         return (
-                        <Link key={item.href} href={item.href}
+                        <Link key={item.href} href={item.href} onClick={() => onOpenChange(false)}
                         className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium">
                             {item.children}
                             </Link>
                         )
-                    })}
+                    })};
+                    <div className="border-t">
+                        <Link href="/sign-in"
+                        className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium">
+                            Log In
+                        </Link>
+                        <Link href="/sign-up"
+                        className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium">
+                            Start Selling
+                        </Link>
+                    </div>
 
                 </ScrollArea>
             </SheetContent>
