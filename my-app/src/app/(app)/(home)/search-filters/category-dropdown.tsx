9167d1ea569465f657488652c7eee70dd04acafd
boben195 <boben195@ukr.net>
2025-08-06 10:28:09 +0300

    add dropdown

--- my-app/src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ my-app/src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -1,5 +1,11 @@
+"use client"
+
+import { useRef, useState } from "react";
+
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import {Category} from "@payload-types"
+
 
 interface Props {
     category: Category;
@@ -9,9 +15,42 @@ interface Props {
 
 
 export const CategoryDropdown = ({ category, isActive, isNavigationHovered }: Props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    const onMouseEnter = () => {
+        if (category.subcategories) {
+            setIsOpen(true)
+        }
+    }
+
+    const onMouseLeave = () => setIsOpen(false);
+
     return (
-        <Button variant="elaveted">
-            {category.name}
-        </Button>
+        <div className="relative"
+            ref={dropdownRef}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+        >
+            <div className="relative">
+                <Button variant="elaveted"
+                   className={cn("h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
+                   isActive && !isNavigationHovered && "bg-white border-primary"
+                )}
+               >
+                     {category.name}
+                </Button>
+                {category.subcategories && category.subcategories.length > 0 && (
+                    <div
+                        className={cn(
+                            "opacity-0 absolute -bottom-3 w-0 h-0 border-l-[10px] border-b-[10px] border-r-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
+                            isOpen && "opacity-100"
+                        )}
+                    />
+
+                    
+                )}
+            </div>
+        </div>
     )
 }
