30133ab46f258be2b5316f13515751ca63e00c94
boben195 <boben195@ukr.net>
2025-08-06 15:55:41 +0300

    continue with sub menu

--- my-app/src/app/(app)/(home)/search-filters/subcategory-menu.tsx
+++ my-app/src/app/(app)/(home)/search-filters/subcategory-menu.tsx
@@ -16,4 +16,21 @@ export const SubcategoryMenu = ({
     if (!isOpen || !category.subcategories || category.subcategories.length === 0) {
         return null
     }
+
+    const backgroundColor = category.color || "#F5F5F5"
+
+    return (
+        <div className="fixed z-100"
+            style={{
+                top: position.top,
+                left: position.left,
+            }}
+        >
+            {/*Invisible bridge to maintain hover */}
+            <div className="h-3 w-60" />
+            <div className="w-60 text-black rounded-md overflow-hidden border">
+                <p>Subcategory menu</p>
+            </div>
+        </div>
+    )
 }
